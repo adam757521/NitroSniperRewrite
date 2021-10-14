70d81320b9ff00210302f0e139e47bc8b5a97131
adam7100 <73548219+adam757521@users.noreply.github.com>
2021-10-15 00:52:40 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -13,4 +13,4 @@ class Delay:
 
 class Webhook:
     URL = ""
-    FILTER = [NitroServerResponse.CLAIMED]  # Can be an empty list for all.
+    FILTER = [NitroServerResponse.CLAIMED]  # Can be an empty list for all, when using heroku, please set this to 6 for claimed.
